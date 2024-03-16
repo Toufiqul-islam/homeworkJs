4648cac368e268162092558a007a03c43c967560
Toufiqul-islam <toufiqulislam8@gmail.com>
2024-03-16 14:26:17 +0600

    update

--- homework.js
+++ homework.js
@@ -6,4 +6,17 @@ console.log("Strings Array:", stringsArray);
 let numbersArray = A.filter(item => typeof item === 'number').sort((a,b) => a-b);
 console.log("Shorted Nubers Array:", numbersArray);
 
-string
+let reversedStringArray = stringsArray.slice().reverse();
+console.log("Reversed string array:",reversedStringArray);
+
+let combinedArray = [...numbersArray,...stringsArray];
+console.log("Combined array:", combinedArray);
+
+combinedArray.splice(5,3);
+console.log("Combined array after deletion:", combinedArray);
+
+let lessThan100 = A.filter(item => typeof item === 'number' && item < 100);
+console.log("Elements from A less than 100;", lessThan100);
+
+let modifiedStringsArray = stringsArray.map(str => 'str' + str.slice(0, -1));
+console.log("Modified string array:", modifiedStringsArray);
